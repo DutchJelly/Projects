cb77d52787c57705c474adf730c375a2ad791761
Jelle Keulemans <jelle.keulemans@gmail.com>
2018-09-28 17:58:28 +0200

    Finished assignment and fixed bug

--- PMOpdrachten4.cc
+++ PMOpdrachten4.cc
@@ -18,11 +18,11 @@ void telBreukOp(int teller1, int noemer1, int teller2, int noemer2, int & atelle
 }
 //Opdracht 4
 double macht(double nummer, int macht){
-    nummer = 0;
-    for(; macht > 0; macht--){
-        nummer *= nummer;
+    double returnValue = 1;
+    for(;macht > 0; macht--){
+        returnValue *= nummer;
     }
-    return nummer;
+    return returnValue;
 }
 //Opdracht 5
 int splitInRegels(int nummer){
@@ -36,23 +36,42 @@ int splitInRegels(int nummer){
     }
     return resultaat;
 }
-//Opdracht 6+7+8
+//Opdracht 10
+int addDigitOnRight(int number, int add){
+    return (number * 10) + add;
+}
+//Opdracht 11
+void verwerkGetal(int & currentNumber, bool & digitFound){
+    cout << "Squared encountered number: " << macht(currentNumber,2) << endl;
+    currentNumber = 0;
+    digitFound = false;
+}
+
+//Opdracht 6+7+8+9+10
 void copieerBestand(){
     int charCount = 0;
     int lineFeed = 0;
+    //Wijzig path zo nodig.
     ifstream input;
-    input.open("/home/jellekeul/Projects/invoer.txt");
+    input.open("/home/jellekeul/git-repositories/Projects/invoer.txt");
     ofstream output;
-    output.open("/home/jellekeul/Projects/uitvoer.txt");
+    output.open("/home/jellekeul/git-repositories/Projects/uitvoer.txt");
     char c = input.get();
+    int currentNumber = 0;
+    bool digitFound = false;
     while(!input.eof()){
-        if(isdigit(c)) cout << c << endl;
+        if(isdigit(c)){
+            if(!digitFound) digitFound = true;
+            currentNumber = addDigitOnRight(currentNumber, (int)c - 48);
+        } else if(digitFound){
+            verwerkGetal(currentNumber, digitFound);
+        }
         if(c == '\n') lineFeed++;
         else charCount ++;
         output.put(c);
         c = input.get();
     }
-    cout << "Characters: " << charCount << "\n" << "File copy succeeded!" << endl;
+    cout << "Characters: " << charCount << "\nLinefeed: " << lineFeed << "\nFile copy succeeded!" << endl;
 }
 int main(){
     copieerBestand();
