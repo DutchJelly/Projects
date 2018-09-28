47aeda98561608a7e746a196150a4c306d734ed5
Jelle Keulemans <jelle.keulemans@gmail.com>
2018-09-28 18:33:27 +0200

    added some assignments

--- PMOpdrachten4.cc
+++ PMOpdrachten4.cc
@@ -46,7 +46,6 @@ void verwerkGetal(int & currentNumber, bool & digitFound){
     currentNumber = 0;
     digitFound = false;
 }
-
 //Opdracht 6+7+8+9+10
 void copieerBestand(){
     int charCount = 0;
@@ -73,7 +72,55 @@ void copieerBestand(){
     }
     cout << "Characters: " << charCount << "\nLinefeed: " << lineFeed << "\nFile copy succeeded!" << endl;
 }
+//opdracht 14
+char toUpperCase(char c){
+    int asciiIndex = (int)c;
+    if(asciiIndex >= 97 && asciiIndex <= 122)
+        return (char)(asciiIndex - 32);
+    return c;
+}
+//opdracht 15
+char returnFirstAlphabetically(char c1, char c2){
+    return (int)c1 < (int)c2 ? c1 : c2;
+}
+//opdracht 16
+void swapParameters(int & p1, int & p2){
+    int p1Copy = p1;
+    p1 = p2;
+    p2 = p1Copy;
+}
+//opdracht 17
+void printMultiplicates(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << i*j << "  ";
+        }
+        cout << endl;
+    }
+}
+//Opdracht 18
+void printCallAmount(){
+    static int amount = 0;
+    amount++;
+    cout << "I've been called " << amount << " times!" << endl;
+}
+//Opdracht 19
+void followFormla(int startWith, int continueFor, bool printBetweenValues){
+    //using formula f(x) = 3x+1
+    if(printBetweenValues){
+        for(int i = 0; i < continueFor; i++){
+            cout << (3*(i+startWith) + 1) << endl;
+        }
+    }
+    cout << (3*(continueFor+startWith) + 1) << endl;
+}
+//Only used to test functions.
 int main(){
-    copieerBestand();
+    int input;
+    cout << "Enter number: ";
+    cin >> input;
+    cout << endl;
+    printMultiplicates(input);
+
     return 1;
 }
